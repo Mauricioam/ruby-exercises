15b2acd34632fa66942baba8d52fbc943371b81d
Mauricioam <mauricioam91@gmail.com>
2026-01-23 22:29:57 +0000

    resolve pet_pun

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -30,6 +30,10 @@ end
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+def lucky_number(number = 7)
+ "Today's lucky number is #{number}"
+end
+
 
 
 # method name: #ascii_code
@@ -37,7 +41,12 @@ end
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
-
+def ascii_code(character)
+  unless (character.length == 1)
+    return "Input Error"
+  end
+  character.ord
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -46,10 +55,23 @@ end
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
-
+def pet_pun(animal)
+  if(animal == "cat")
+    puts "Cats are purr-fect!"
+  elsif(animal == "dog")
+    puts "Dogs are paw-some!"
+  else
+    puts"I think #{animal}s have pet-tential!"
+  end
+  
+end
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
+
+def twenty_first_century?(year)
+  year.between(2001,2100)
+end
 
